bc7eac8d9b39a34bc734f0c8bf9d4f64b0ca3a66
JohnParksUCD <john.parks@ucdconnect.ie>
2020-03-03 12:22:38 +0000

    Update dynamic_bikes_v2.py
    Updated script so that dynamic bike data is refreshed in the database every five minutes. Also separated timestamp into date and time columns.

--- dynamic_bikes_v2.py
+++ dynamic_bikes_v2.py
@@ -17,6 +17,7 @@ import os
 import time
 from IPython.display import display
 import mysql.connector
+from datetime import datetime
 
 #set up the database connection
 URI = "database-1.cx36tayg9smy.us-east-1.rds.amazonaws.com"
@@ -32,20 +33,20 @@ APIKEY = "eb2ccb1179e38c5251aac92c5182497cd4e7222d"
 NAME = "Dublin"
 STATIONS_URI = "https://api.jcdecaux.com/vls/v1/stations"
 
-r = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
-pprint(r)
 
 sql =  """CREATE TABLE IF NOT EXISTS DynamicBike (
         available_bike_stands INTEGER,
         available_bikes INTEGER,
         bike_stands INTEGER,
         name VARCHAR(256),
-        number INTEGER,
-        last_update TIMESTAMP,
-        status VARCHAR(256))"""
+        number INTEGER NOT NULL,
+        date_update DATE NOT NULL,
+        time_update TIME NOT NULL,
+        status VARCHAR(256),
+        PRIMARY KEY(number, date_update, time_update))"""
 
 try:
-    res = engine.execute("DROP TABLE IF EXISTS DynamicBike")
+    #res = engine.execute("DROP TABLE IF EXISTS DynamicBike")
     res = engine.execute(sql)
     print(res.fetchall())
     
@@ -60,10 +61,21 @@ def dynamic_bikes(text):
         int(DynamicBike.get("bike_stands")),
         DynamicBike.get("name"),
         int(DynamicBike.get("number")),
-        DynamicBike.get("last_update"),
+        datetime.now(),
+        datetime.now(),         
         DynamicBike.get("status"))
         
-        engine.execute("insert into DynamicBike values(%s, %s, %s, %s, %s, %s, %s)", vals)
+        
+        engine.execute("insert into DynamicBike values(%s, %s, %s, %s, %s, %s, %s, %s)", vals)
     return
 
-dynamic_bikes(r.text)
+while True:
+    try:
+        r = requests.get(STATIONS_URI, params={"apiKey": APIKEY, "contract": NAME})
+        dynamic_bikes(r.text)
+        
+        #sleep for 5 mins
+        time.sleep(5*60)
+    except Exception as e:
+        #If there is a problem print traceback
+        print(e)
