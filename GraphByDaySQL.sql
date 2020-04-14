7072514b2220af351fb332adef73b76bf71bef74
JohnParksUCD <john.parks@ucdconnect.ie>
2020-04-14 10:07:51 +0100

    Updating graph_data and graphSQL
    There was an issue when the dynamic bikes table was updated at exactly midnight, now fixed

--- GraphByDaySQL.sql
+++ GraphByDaySQL.sql
@@ -1,5 +1,4 @@
--- Query to insert data for graph grouped by station, day of the week and hour of the day
 insert into SE_Project.GraphByDay
-SELECT d.number, d.name, (weekday(d.date_update)) as weekday, (d.available_bikes), avg(d.available_bike_stands), time_to_sec(d.time_update) div (60*60)  as time_update 
+SELECT d.number, d.name, (weekday(d.date_update)) as weekday, (d.available_bikes), avg(d.available_bike_stands), least(time_to_sec(d.time_update) div (60*60), 23)  as time_update 
 FROM SE_Project.DynamicBike d
-group by d.number, d.name, weekday(d.date_update), time_to_sec(d.time_update) div (60*60);
+group by d.number, d.name, weekday(d.date_update), least(time_to_sec(d.time_update) div (60*60), 23);
--- graph_data.py
+++ graph_data.py
@@ -34,9 +34,9 @@ except Exception as e:
     
 #SQL query to insert data into graphByDay table
 sql2 = """insert into SE_Project.GraphByDay
-SELECT d.number, d.name, (weekday(d.date_update)) as weekday, (d.available_bikes), avg(d.available_bike_stands), time_to_sec(d.time_update) div (60*60)  as time_update 
+SELECT d.number, d.name, (weekday(d.date_update)) as weekday, (d.available_bikes), avg(d.available_bike_stands), least(time_to_sec(d.time_update) div (60*60), 23)  as time_update 
 FROM SE_Project.DynamicBike d
-group by d.number, d.name, weekday(d.date_update), time_to_sec(d.time_update) div (60*60)"""
+group by d.number, d.name, weekday(d.date_update), least(time_to_sec(d.time_update) div (60*60), 23)"""
 
 #truncate GraphByDay table and insert rows into it, if there is an error throw an exception
 try:
