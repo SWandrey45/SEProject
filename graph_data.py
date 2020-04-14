dc9f9194030346685f3850d4fe50dc42ec7ea9c2
JohnParksUCD <john.parks@ucdconnect.ie>
2020-04-14 11:18:39 +0100

    Update grap_data and GraphByDay
    Update to fix bug

--- graph_data.py
+++ graph_data.py
@@ -34,7 +34,7 @@ except Exception as e:
     
 #SQL query to insert data into graphByDay table
 sql2 = """insert into SE_Project.GraphByDay
-SELECT d.number, d.name, (weekday(d.date_update)) as weekday, (d.available_bikes), avg(d.available_bike_stands), least(time_to_sec(d.time_update) div (60*60), 23)  as time_update 
+SELECT d.number, d.name, (weekday(d.date_update)) as weekday, avg(d.available_bikes), avg(d.available_bike_stands), least(time_to_sec(d.time_update) div (60*60), 23)  as time_update 
 FROM SE_Project.DynamicBike d
 group by d.number, d.name, weekday(d.date_update), least(time_to_sec(d.time_update) div (60*60), 23)"""
 
